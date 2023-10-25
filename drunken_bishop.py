8ca9a4db5b11773d548237675447071a6b8eeae1
roadmaus <47949813+roadmaus@users.noreply.github.com>
2023-10-25 13:51:18 +0200

    Update drunken_bishop.py

--- drunken_bishop.py
+++ drunken_bishop.py
@@ -61,7 +61,62 @@ def room_to_string(room):
         output += "|" + "".join(line) + "|\n"
     output += "+" + "-" * RoomWidth + "+"
     return output
+# New Start and End Characters for second bishop
+StartChar2 = 'A'
+EndChar2 = 'Z'
+StartCode2 = 3000
+EndCode2 = 4000
 
+# Function to move a single bishop
+def move_bishop(room, pos, StartCode, EndCode, input_bytes):
+    for byte in input_bytes:
+        bits = format(byte, '08b')
+        bitpairs = [bits[i:i+2] for i in range(0, 8, 2)]
+        for bitpair in bitpairs:
+            direction = list(DIRECTIONS.keys())[int(bitpair, 2) % 8]
+            dY, dX = DIRECTIONS[direction]
+            pos[0] += dY
+            pos[1] += dX
+            pos[0] = max(0, min(RoomHeight - 1, pos[0]))
+            pos[1] = max(0, min(RoomWidth - 1, pos[1]))
+            room[pos[0]][pos[1]] += 1
+    room[pos[0]][pos[1]] = EndCode
+    return room
+
+def from_bytes(input_bytes1, input_bytes2):
+    room = [[0 for _ in range(RoomWidth)] for _ in range(RoomHeight)]
+    pos1 = [random.randint(0, RoomHeight - 1), random.randint(0, RoomWidth - 1)]
+    pos2 = [random.randint(0, RoomHeight - 1), random.randint(0, RoomWidth - 1)]
+    room[pos1[0]][pos1[1]] = StartCode
+    room[pos2[0]][pos2[1]] = StartCode2
+
+    room = move_bishop(room, pos1, StartCode, EndCode, input_bytes1)
+    room = move_bishop(room, pos2, StartCode2, EndCode2, input_bytes2)
+    
+    return room
+
+def room_to_string(room):
+    output = "+" + "-" * RoomWidth + "+\n"
+    for row in room:
+        line = []
+        for col in row:
+            if col == StartCode:
+                char = StartChar
+            elif col == EndCode:
+                char = EndChar
+            elif col == StartCode2:
+                char = StartChar2
+            elif col == EndCode2:
+                char = EndChar2
+            elif col < len(Alphabet):
+                char = Alphabet[col]
+            else:
+                char = UnknownChar
+            line.append(char)
+        output += "|" + "".join(line) + "|\n"
+    output += "+" + "-" * RoomWidth + "+"
+    return output
+    
 def write_to_file(room_string):
     # Create folder if it doesn't exist
     folder_name = "random_patterns"
@@ -78,7 +133,8 @@ def write_to_file(room_string):
         f.write(room_string)
 
 # Test the code
-random_bytes = [random.randint(0, 255) for _ in range(200)]
-room = from_bytes(random_bytes)
+random_bytes1 = [random.randint(0, 255) for _ in range(200)]
+random_bytes2 = [random.randint(0, 255) for _ in range(200)]
+room = from_bytes(random_bytes1, random_bytes2)
 room_string = room_to_string(room)
 write_to_file(room_string)
