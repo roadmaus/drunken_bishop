4bf6721d81fd04f6ea5029b3ce7cbaa6ec169e6f
roadmaus <47949813+roadmaus@users.noreply.github.com>
2023-10-25 14:12:58 +0200

    Update drunken_bishop.py

--- drunken_bishop.py
+++ drunken_bishop.py
@@ -1,18 +1,23 @@
 import os
 import random
+import argparse
+
+# Argument Parsing
+parser = argparse.ArgumentParser(description='Generate random ASCII art pattern with multiple bishops.')
+parser.add_argument('--min-bishops', type=int, default=2, help='Minimum number of bishops')
+parser.add_argument('--max-bishops', type=int, default=10, help='Maximum number of bishops')
+
+args = parser.parse_args()
 
 RoomWidth = 100
 RoomHeight = 80
 Alphabet = " .o+=*BOX@%&#/^"
 UnknownChar = '!'
-StartY = RoomHeight // 2
-StartX = RoomWidth // 2
 StartCode = 1000
 EndCode = 2000
 StartChar = 'S'
 EndChar = 'E'
 
-# Now with eight directions, including N, E, S, W
 DIRECTIONS = {
     "NW": [-1, -1],
     "NE": [-1, 1],
@@ -24,24 +29,33 @@ DIRECTIONS = {
     "W": [0, -1]
 }
 
-def from_bytes(input_bytes):
+def from_bytes(input_bytes, num_bishops):
     room = [[0 for _ in range(RoomWidth)] for _ in range(RoomHeight)]
-    pos = [StartY, StartX]
+
+    bishop_positions = [[random.randint(0, RoomHeight - 1), random.randint(0, RoomWidth - 1)] for _ in range(num_bishops)]
     
     for byte in input_bytes:
         bits = format(byte, '08b')
         bitpairs = [bits[i:i+2] for i in range(0, 8, 2)]
-        for bitpair in bitpairs:
-            direction = list(DIRECTIONS.keys())[int(bitpair, 2) % 8]
-            dY, dX = DIRECTIONS[direction]
-            pos[0] += dY
-            pos[1] += dX
-            pos[0] = max(0, min(RoomHeight - 1, pos[0]))
-            pos[1] = max(0, min(RoomWidth - 1, pos[1]))
-            room[pos[0]][pos[1]] += 1
+        
+        for pos in bishop_positions:
+            for bitpair in bitpairs:
+                direction = list(DIRECTIONS.keys())[int(bitpair, 2) % 8]
+                dY, dX = DIRECTIONS[direction]
+                pos[0] += dY
+                pos[1] += dX
+                pos[0] = max(0, min(RoomHeight - 1, pos[0]))
+                pos[1] = max(0, min(RoomWidth - 1, pos[1]))
+                room[pos[0]][pos[1]] += 1
 
-    room[StartY][StartX] = StartCode
-    room[pos[0]][pos[1]] = EndCode
+    for i, pos in enumerate(bishop_positions):
+        if i == 0:
+            room[pos[0]][pos[1]] = StartCode
+        elif i == num_bishops - 1:
+            room[pos[0]][pos[1]] = EndCode
+        else:
+            room[pos[0]][pos[1]] = random.randint(StartCode + 1, EndCode - 1)
+    
     return room
 
 def room_to_string(room):
@@ -61,80 +75,21 @@ def room_to_string(room):
         output += "|" + "".join(line) + "|\n"
     output += "+" + "-" * RoomWidth + "+"
     return output
-# New Start and End Characters for second bishop
-StartChar2 = 'A'
-EndChar2 = 'Z'
-StartCode2 = 3000
-EndCode2 = 4000
 
-# Function to move a single bishop
-def move_bishop(room, pos, StartCode, EndCode, input_bytes):
-    for byte in input_bytes:
-        bits = format(byte, '08b')
-        bitpairs = [bits[i:i+2] for i in range(0, 8, 2)]
-        for bitpair in bitpairs:
-            direction = list(DIRECTIONS.keys())[int(bitpair, 2) % 8]
-            dY, dX = DIRECTIONS[direction]
-            pos[0] += dY
-            pos[1] += dX
-            pos[0] = max(0, min(RoomHeight - 1, pos[0]))
-            pos[1] = max(0, min(RoomWidth - 1, pos[1]))
-            room[pos[0]][pos[1]] += 1
-    room[pos[0]][pos[1]] = EndCode
-    return room
-
-def from_bytes(input_bytes1, input_bytes2):
-    room = [[0 for _ in range(RoomWidth)] for _ in range(RoomHeight)]
-    pos1 = [random.randint(0, RoomHeight - 1), random.randint(0, RoomWidth - 1)]
-    pos2 = [random.randint(0, RoomHeight - 1), random.randint(0, RoomWidth - 1)]
-    room[pos1[0]][pos1[1]] = StartCode
-    room[pos2[0]][pos2[1]] = StartCode2
-
-    room = move_bishop(room, pos1, StartCode, EndCode, input_bytes1)
-    room = move_bishop(room, pos2, StartCode2, EndCode2, input_bytes2)
-    
-    return room
-
-def room_to_string(room):
-    output = "+" + "-" * RoomWidth + "+\n"
-    for row in room:
-        line = []
-        for col in row:
-            if col == StartCode:
-                char = StartChar
-            elif col == EndCode:
-                char = EndChar
-            elif col == StartCode2:
-                char = StartChar2
-            elif col == EndCode2:
-                char = EndChar2
-            elif col < len(Alphabet):
-                char = Alphabet[col]
-            else:
-                char = UnknownChar
-            line.append(char)
-        output += "|" + "".join(line) + "|\n"
-    output += "+" + "-" * RoomWidth + "+"
-    return output
-    
 def write_to_file(room_string):
-    # Create folder if it doesn't exist
     folder_name = "random_patterns"
     if not os.path.exists(folder_name):
         os.mkdir(folder_name)
 
-    # Find the next available file name
     i = 1
     while os.path.exists(f"{folder_name}/random_{i}.txt"):
         i += 1
 
-    # Write to the file
     with open(f"{folder_name}/random_{i}.txt", "w") as f:
         f.write(room_string)
 
-# Test the code
-random_bytes1 = [random.randint(0, 255) for _ in range(200)]
-random_bytes2 = [random.randint(0, 255) for _ in range(200)]
-room = from_bytes(random_bytes1, random_bytes2)
+num_bishops = random.randint(args.min_bishops, args.max_bishops)
+random_bytes = [random.randint(0, 255) for _ in range(200)]
+room = from_bytes(random_bytes, num_bishops)
 room_string = room_to_string(room)
 write_to_file(room_string)
